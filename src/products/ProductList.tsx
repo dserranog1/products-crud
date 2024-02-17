35e27005b60c353bf03ed3239083baf28145a2bf
Daniel Serrano <dserranog@unal.edu.co>
2024-02-16 19:11:57 -0500

    feat: add sorting by price

--- src/products/ProductList.tsx
+++ src/products/ProductList.tsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import SearchBar from "./components/SearchBar";
 import { useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const filterProductsFn = (product: Product, query: string): boolean => {
   return (
@@ -16,11 +17,31 @@ const filterProductsFn = (product: Product, query: string): boolean => {
     .includes(query.toLowerCase());
 };
 
+const compareProductAsc = (productA: Product, productB: Product) => {
+  if (productA.price > productB.price) {
+    return 1
+  }
+  if (productA.price < productB.price) {
+    return -1
+  }
+  return 0
+}
+const compareProductDesc = (productA: Product, productB: Product) => {
+  if (productA.price > productB.price) {
+    return -1
+  }
+  if (productA.price < productB.price) {
+    return 1
+  }
+  return 0
+}
+
 
 const ProductList = () => {
    const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [isFiltered, setIsFiltered] = useState(false);
   const [searchBarValue, setSearchBarValue] = useState<string>("");
+  const [orderByValue, setOrderByValue] = useState<string>("asc");
   const {
     data: products,
     isError,
@@ -36,9 +57,13 @@ const ProductList = () => {
   if (isError || !isSuccess) {
     return <div>woops! error loading the data</div>;
   }
+
+
     const currentListOfProducts = isFiltered
       ? filteredProducts
       : products
+
+    currentListOfProducts.sort(orderByValue === 'asc' ? compareProductAsc : compareProductDesc)
 
   return (
     <div className="flex flex-col gap-4">
@@ -53,6 +78,19 @@ const ProductList = () => {
         data={products}
         filterFn={filterProductsFn}
       />
+      <Select onValueChange={(value) => setOrderByValue(value)} defaultValue={orderByValue}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select one" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="asc">
+            Order by price ascending
+          </SelectItem>
+          <SelectItem value="desc">
+            Order by price descending
+          </SelectItem>
+        </SelectContent>
+      </Select>
       <div className="grid grid-cols-4">
         {currentListOfProducts.map((product) => {
           return (
